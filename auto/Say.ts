03a4f66988f15143c3b0918abc574962857729e6
titancube <kbin0717@gmail.com>
2021-03-10 23:15:32 +0900

    feat: Markov chain impersonator

--- auto/Say.ts
+++ auto/Say.ts
@@ -1,16 +1,28 @@
 import { Command, CommandMessage } from "@typeit/discord";
-import * as Markov from '@0x77/markov-typescript'
 import db from '../plugins/firebase'
+import { MarkovChain } from "@0x77/markov-typescript";
 
 export abstract class Say {
     @Command("say :person :number")
-    async saus(command: CommandMessage): Promise<void> {
-        const personToImpersonate = command.args.person
+    async say(command: CommandMessage): Promise<void> {
+        const impersonatee = command.args.person
         const msgCount = command.args.number
-        console.log(command.args.person)
 
+        console.log(impersonatee)
 
-        // db.collection('Member').doc(command.args.person)
-        // command.channel.send('gratis saus')
+        if (msgCount > 100 || msgCount < 0) {
+            command.channel.send('Message count cannot be higher than 100 or lower than 0')
+            return
+        }
+
+        const samples = await db.collection('Member').doc(impersonatee).collection('Messages').orderBy('createdAt', 'desc').limit(msgCount).get()
+        const chain = new MarkovChain<string>(msgCount);
+        samples.forEach(r => {
+            chain.learn(r.data().message.split(" "));
+        })
+
+        for (let x = 0; x < Math.floor(Math.random() * 100); x++) {
+            command.channel.send(chain.walk().join(" "));
+        }
     }
 }
