e34b662e6ef7832d0874b1f6e83491b9ebeb0bd6
titancube <kbin0717@gmail.com>
2021-03-10 21:35:45 +0900

    test: log user id

---
+++ auto/Say.ts
@@ -0,0 +1,13 @@
+import { Command, CommandMessage } from "@typeit/discord";
+import db from '../plugins/firebase'
+
+export abstract class Say {
+    @Command("say :person :number")
+    async saus(command: CommandMessage): Promise<void> {
+        const personToImpersonate = command.args.person
+        const msgCount = command.args.number
+        console.log(command.args.person)
+        // db.collection('Member').doc(command.args.person)
+        // command.channel.send('gratis saus')
+    }
+}
