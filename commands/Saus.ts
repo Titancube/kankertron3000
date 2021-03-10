924bfe4666c5869b5105018df01b6e2ea6eb0ee7
titancube <kbin0717@gmail.com>
2021-03-10 21:36:01 +0900

    refactor: clean up

--- commands/Saus.ts
+++ commands/Saus.ts
@@ -1,11 +1,8 @@
 import { Command, CommandMessage } from "@typeit/discord";
-import db from '../plugins/firebase'
-import { format, formatDistanceToNowStrict } from 'date-fns'
-import ko from 'date-fns/locale/ko'
 
-export abstract class Hello {
+export abstract class Saus {
     @Command("saus")
-    async retire(command: CommandMessage): Promise<void> {
+    async saus(command: CommandMessage): Promise<void> {
         command.channel.send('gratis saus')
     }
 }
