b02db2956c2818d86bbd54a7422298c3d2dbd8c6
titancube <kbin0717@gmail.com>
2021-03-10 21:36:29 +0900

    feat: add automatically executed functions

--- discords/DiscordApp.ts
+++ discords/DiscordApp.ts
@@ -3,12 +3,15 @@ import * as Path from "path";
 
 @Discord("$", {
   import: [
-    Path.join(__dirname, "..", "commands", "*.ts")
+    Path.join(__dirname, "..", "commands", "*.ts"),
+    Path.join(__dirname, "..", "auto", "*.ts")
   ]
 })
+
 export class DiscordApp {
   @CommandNotFound()
-  notFoundA(command: CommandMessage): void {
+  notFound(command: CommandMessage): void {
     command.channel.send("I got no saus for that");
   }
+
 }
