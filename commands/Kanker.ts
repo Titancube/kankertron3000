2b6f255f1ef96b98749fc639ae03eff29b965e70
titancube <kbin0717@gmail.com>
2021-03-30 01:31:25 +0900

    fix: euler made us to wait until he actually says

--- commands/Kanker.ts
+++ commands/Kanker.ts
@@ -10,23 +10,37 @@ export abstract class Kanker {
   })
   private async kanker(command: CommandMessage): Promise<void> {
     const vc = command.member.voice.channel;
+
+    const getRndBias = (
+      min: number,
+      max: number,
+      bias: number,
+      influence: number
+    ) => {
+      const rnd = Math.random() * (max - min) + min, // random in range
+        mix = Math.random() * influence; // random mixer
+      return rnd * (1 - mix) + bias * mix; // mix full range and bias
+    };
+
+    const theTime = getRndBias(1000, 120000, 2000, 0.75);
+
     if (vc) {
       const r = await vc.join();
-      const dispatcher = r.play(
-        path.join(__dirname, "../assets/audio/kanker.wav")
-      );
-
-      dispatcher.setVolume(0.75);
-
-      dispatcher
-        .on("finish", () => {
-          console.log("DONE");
-          dispatcher.destroy();
-          command.guild.me.voice.channel.leave();
-        })
-        .on("error", (e) => {
-          console.log(e);
-        });
+      const play = async () => {
+        const dispatcher = r.play(
+          path.join(__dirname, "../assets/audio/kanker.wav")
+        );
+        dispatcher.setVolume(0.65);
+        dispatcher
+          .on("finish", () => {
+            dispatcher.destroy();
+            command.guild.me.voice.channel.leave();
+          })
+          .on("error", (e) => {
+            console.log(e);
+          });
+      };
+      setTimeout(play, theTime);
     } else {
       command.channel.send(
         "Join voice channel to execute this horrifying function"
