34b522f00c2fbbc5e57cfd661f2282a2a7d0244f
titancube <kbin0717@gmail.com>
2021-03-30 00:41:53 +0900

    feat: play kanker.wav

---
+++ commands/Kanker.ts
@@ -0,0 +1,34 @@
+import { Command, CommandMessage, Infos } from "@typeit/discord";
+import * as path from "path";
+
+export abstract class Kanker {
+  @Command("kanker")
+  @Infos({
+    command: `kanker`,
+    detail: "`$kanker`",
+    description: "* kanker",
+  })
+  private async kanker(command: CommandMessage): Promise<void> {
+    const vc = command.member.voice.channel;
+    if (vc) {
+      const r = await vc.join();
+      const dispatcher = r.play(
+        path.join(__dirname, "../assets/audio/kanker.wav")
+      );
+
+      dispatcher.setVolume(0.75);
+
+      dispatcher
+        .on("finish", () => {
+          console.log("DONE");
+          dispatcher.destroy();
+          command.guild.me.voice.channel.leave();
+        })
+        .on("error", (e) => {
+          console.log(e);
+        });
+    } else {
+      command.channel.send("보이스 채널에 입장해주세요");
+    }
+  }
+}
