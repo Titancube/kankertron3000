753f345a580c400d8a9cf295042c7c109a605fb9
Titancube <kbin0717@gmail.com>
2021-04-07 12:48:25 +0900

    style: commentate

--- auto/Detection.ts
+++ auto/Detection.ts
@@ -3,9 +3,11 @@ import { NotBot } from "../guards/NotBot";
 
 export abstract class Detection {
   @On("message")
-  @Guard(NotBot)
+  @Guard(NotBot) // Message author should not be the bot
   private async textWithoutInfo([msg]: ArgsOf<"message">): Promise<void> {
     // Check the channel - set to #resources-n-sales / "827736673777090620"
+    // Check if the message author has admin permission
+    // Stop if at least one of both is true
     if (
       msg.channel.id !== "827736673777090620" ||
       msg.member.hasPermission("ADMINISTRATOR", { checkAdmin: true })
@@ -13,11 +15,11 @@ export abstract class Detection {
       return;
     }
 
-    // Cancel detection if the message is written by mods
+    // Check if the message has at least one link or attachment
     Detection.hasLink([msg]) || Detection.hasAttachment([msg])
-      ? // Message has a link or attachment
+      ? // Message has a link or attachment, pass the message
         true
-      : // Message does not have a link or an attachment. Delete the message
+      : // Message does not have a link or an attachment. Delete the message and send notification through DM
         (msg.delete(),
         await msg.author.send(
           "Messages on the `#resources-n-sales` should have at least one attachment or link!\nIf this problem keeps occuring please contact **blob(Titancube)** on the server."
@@ -26,11 +28,13 @@ export abstract class Detection {
 
   // Detect if the message has link
   private static hasLink([msg]: ArgsOf<"message">) {
+    // Return true if index of detected url is bigger than -1. String.prototype.search() returns -1 when it finds nothing
     return msg.content.search(Detection.urlRegex) > -1 ? true : false;
   }
 
   // Detect if the message has attachment
   private static hasAttachment([msg]: ArgsOf<"message">) {
+    // Return true if attachment count is bigger than 0
     return msg.attachments.size > 0 ? true : false;
   }
 
