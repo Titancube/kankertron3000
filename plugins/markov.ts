81ba03393d62ed15e83dc735f54ec39b3b378c25
titancube <kbin0717@gmail.com>
2021-04-27 22:11:50 +0900

    fix: cannot declare new instance

--- plugins/markov.ts
+++ plugins/markov.ts
@@ -1,56 +1,55 @@
 // https://medium.com/@alexkrameris/markov-chain-implementation-in-javascript-a698f371d66f
 // My own implementation in typescript of Alex Kramer's original javascript version
 
-export abstract class Markov {
-  private static state: string[] = [];
-  private static markovChain = {};
+export class Markov {
+  private state: string[] = [];
+  private markovChain = {};
 
-  static addState(text: Array<string> | string): void {
+  addState(text: Array<string> | string): void {
     if (Array.isArray(text)) {
       Array.from(text).forEach((v: string) =>
-        v.split(' ').forEach((n: string) => Markov.state.push(Markov.trim(n)))
+        v.split(' ').forEach((n: string) => this.state.push(Markov.trim(n)))
       );
     } else if (typeof text == 'string') {
-      text.split(' ').forEach((v: string) => Markov.state.push(Markov.trim(v)));
+      text.split(' ').forEach((v: string) => this.state.push(Markov.trim(v)));
     }
   }
 
-  private static trim(text: string): string {
+  static trim(text: string): string {
     return text.toLowerCase().replace(/[\W_]/, '');
   }
 
-  static init(): void {
-    Markov.markovChain = {};
+  init(): void {
+    this.markovChain = {};
   }
 
-  static train(): void {
-    const textArr = Markov.state;
+  train(): void {
+    const textArr = this.state;
 
     for (let i = 0; i < textArr.length; i++) {
       const word = textArr[i].toLowerCase().replace(/[\W_]/, '');
-      if (!Markov.markovChain[word]) {
-        Markov.markovChain[word] = [];
+      if (!this.markovChain[word]) {
+        this.markovChain[word] = [];
       }
       if (textArr[i + 1]) {
-        Markov.markovChain[word].push(
+        this.markovChain[word].push(
           textArr[i + 1].toLowerCase().replace(/[\W_]/, '')
         );
       }
     }
   }
 
-  static generate(count: number): string {
-    const words = Object.keys(Markov.markovChain);
+  generate(count: number): string {
+    const words = Object.keys(this.markovChain);
     let word = words[Math.floor(Math.random() * words.length)];
     let result = '';
     for (let i = 0; i < count; i++) {
       result += word + ' ';
-      const newWord =
-        Markov.markovChain[word][
-          Math.floor(Math.random() * Markov.markovChain[word].length)
-        ];
+      const newWord = this.markovChain[word][
+        Math.floor(Math.random() * this.markovChain[word].length)
+      ];
       word = newWord;
-      if (!word || !Markov.markovChain)
+      if (!word || !this.markovChain)
         word = words[Math.floor(Math.random() * count)];
     }
     return result;
