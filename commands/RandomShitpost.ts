06ccda514284dc1ddb7eb84e9360a3c17f783b6f
Titancube <kbin0717@gmail.com>
2021-04-28 10:51:10 +0900

    fix: use bear api call rather than soundcloud.ts

--- commands/RandomShitpost.ts
+++ commands/RandomShitpost.ts
@@ -1,39 +1,45 @@
 import { Command, CommandMessage, Infos } from '@typeit/discord';
-import { randomInt } from 'node:crypto';
-import Soundcloud from "soundcloud.ts";
+import axios from 'axios';
+import { randomInt } from 'crypto';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-export abstract class RandomEoti {
+export abstract class RandomShitpost {
   @Command('randomeoti')
   @Infos({
     command: `randomeoti`,
     detail: '`$randomeoti`',
     description: '* Posts a random EOTI track.',
   })
-  private async randomeoti(command: CommandMessage): Promise<void> {
-    const soundcloud = new Soundcloud();
-    const user = await soundcloud.users.getV2("explorersoftheinternet");
-    const trackCount = await user.track_count;
-    const searchResults = await soundcloud.tracks.searchV2({q: "Explorers of the Internet"});
-    const randomTrackNumber = randomInt(0, trackCount);
-    
-    command.channel.send('Enjoy your free kanker! ' + searchResults.collection[randomTrackNumber].permalink_url);
-  }
-}
+  private async randomEoti(command: CommandMessage): Promise<void> {
+    const api = `https://api.soundcloud.com/tracks?q=Explorers%20of%20the%20Internet&access=playable&limit=50&linked_partitioning=true&client_id=`;
+    const searchResults = await axios.get(api + process.env.SC_CLIENT_ID, {
+      headers: { Authorization: 'Bearer' + process.env.SC_OAUTH_TOKEN },
+    });
+    const randomTrackNumber = randomInt(0, 50);
 
-export abstract class RandomShittypedia {
+    command.channel.send(
+      'Enjoy your free kanker! ' +
+        searchResults.data.collection[randomTrackNumber].permalink_url
+    );
+  }
+
   @Command('randomshittypedia')
   @Infos({
     command: `randomshittypedia`,
     detail: '`$randomshittypedia`',
     description: '* Posts a random Shittypedia track.',
   })
-  private async randomeoti(command: CommandMessage): Promise<void> {
-    const soundcloud = new Soundcloud();
-    const user = await soundcloud.users.getV2("shittypedia");
-    const trackCount = await user.track_count;
-    const searchResults = await soundcloud.tracks.searchV2({q: "Shittypedia"});
-    const randomTrackNumber = randomInt(0, trackCount);
-    
-    command.channel.send('Enjoy your free gangnam style! ' + searchResults.collection[randomTrackNumber].permalink_url);
+  private async randomShittypedia(command: CommandMessage): Promise<void> {
+    const api = `https://api.soundcloud.com/tracks?q=Shittypedia&access=playable&limit=50&linked_partitioning=true&client_id=`;
+    const searchResults = await axios.get(api + process.env.SC_CLIENT_ID, {
+      headers: { Authorization: 'Bearer' + process.env.SC_OAUTH_TOKEN },
+    });
+    const randomTrackNumber = randomInt(0, 50);
+
+    command.channel.send(
+      'Enjoy your free gangnam style! ' +
+        searchResults.data.collection[randomTrackNumber].permalink_url
+    );
   }
 }
