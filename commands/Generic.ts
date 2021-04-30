31dab1c49926c216dcd5c6b7678c8b30e618b551
titancube <kbin0717@gmail.com>
2021-04-30 13:45:16 +0900

    fix: api url

--- commands/Generic.ts
+++ commands/Generic.ts
@@ -41,7 +41,7 @@ export abstract class Say {
         str += '```diff\n- Server Offline\n```';
       }
       command.channel.send(
-        'http://mcapi.us/server/image?ip=blockgame.invertedsilence.com&theme=dark&title=LoudnessRefuge' +
+        'http://mcapi.us/server/image?ip=blockgame.invertedsilence.com&theme=dark&title=Loudness%20Refuge' +
           '&time=' +
           new Date().getTime()
       );
