cc199931d8986ea73a6b38bd736e9e16da3aef47
titancube <kbin0717@gmail.com>
2021-09-28 14:58:11 +0900

    delete: deprecated

--- guards/IsRefugee.ts
+++
@@ -1,9 +0,0 @@
-import { GuardFunction } from "@typeit/discord";
-
-export const IsRefugee: GuardFunction<"message"> = async (
-    [message], client, next
-) => {
-    if (message.guild.id === "421792855506419714") {
-        await next()
-    }
-}
