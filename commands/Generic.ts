60b4eb4c274c90eebae33248b1516eb12000baf3
titancube <kbin0717@gmail.com>
2021-09-28 14:42:15 +0900

    refactor: rewrite code temp: commentate 'say' due to slash option limitation

--- commands/Generic.ts
+++ commands/Generic.ts
@@ -1,20 +1,16 @@
-import { Command, CommandMessage, Infos } from '@typeit/discord';
-import db from '../plugins/firebase';
-import { Markov } from '../plugins/markov';
+import { Discord, Slash } from 'discordx';
 import * as axiosTemp from 'axios';
-import { Validate } from '../plugins/tools';
+import { CommandInteraction } from 'discord.js';
 // import { randomInt } from 'crypto';
 const axios = axiosTemp.default;
 
-export abstract class Say {
+@Discord()
+export abstract class Generic {
   // Get current players from minecraft server
-  @Command('mc')
-  @Infos({
-    command: 'mc',
-    detail: '`$mc`',
-    description: '* Get the details about Loudness Refugee Minecraft server',
+  @Slash('mc', {
+    description: 'Get the details about Loudness Refugee Minecraft Server',
   })
-  async getMcStatus(command: CommandMessage): Promise<void> {
+  async getMcStatus(interaction: CommandInteraction): Promise<void> {
     try {
       const res = await axios.get(
         'https://mcapi.us/server/status?ip=blockgame.invertedsilence.com'
@@ -34,20 +30,22 @@ export abstract class Say {
         });
 
         str +=
-          '\n\n```diff\nCurrent Players\n\n' +
+          '\n\n``diff\nCurrent Players\n\n' +
           activeUsersCount +
           ' Playing\n\n' +
           activePlayers +
-          '```';
+          '``';
       } else {
         str += '```diff\n- Server Offline\n```';
       }
-      command.channel.send(
-        'http://mcapi.us/server/image?ip=blockgame.invertedsilence.com&theme=dark&title=Loudness%20Refuge' +
-          '&time=' +
-          new Date().getTime()
-      );
-      command.channel.send(str);
+      if (interaction.channel) {
+        interaction.channel.send(
+          'http://mcapi.us/server/image?ip=blockgame.invertedsilence.com&theme=dark&title=Loudness%20Refuge' +
+            '&time=' +
+            new Date().getTime()
+        );
+        interaction.reply({ content: str, ephemeral: false });
+      }
     } catch (error) {
       console.log(`[${new Date()}] ERROR`);
       console.log(error);
@@ -55,131 +53,57 @@ export abstract class Say {
   }
 
   // say saus
-  @Command('saus')
-  @Infos({
-    command: `saus`,
-    detail: '`$saus`',
-    description: '* Gratis Saus',
-  })
-  async saus(command: CommandMessage): Promise<void> {
-    command.channel.send('gratis saus');
+  @Slash('saus', { description: 'Gratis Saus' })
+  async saus(interaction: CommandInteraction): Promise<void> {
+    interaction.reply({ content: 'gratis saus', ephemeral: false });
   }
 
-  @Command('donate')
-  @Infos({
-    command: `donate`,
-    detail: '`$donate`',
-    description: '* Donate to Titancube! https://paypal.me/titancube',
-  })
-  private async donate(command: CommandMessage): Promise<void> {
-    command.channel.send(`
-Donate to Titancube for more features! ➡ https://paypal.me/titancube
-    `);
-  }
-
+  // TODO - Rewrite the function
   // imitates target person
-  @Command('say :tempPerson :tempCount')
-  @Infos({
-    command: `say`,
-    detail: `\`$say <@Mention> <Count?>\``,
-    description:
-      "* Speaks randomly generated sentence depends on the mentioned person's chat history\n* You can optionally make how much long the message will be with <Count>\n* Note that the length of the message would not exactly match if the maximum length of the message that targeted person has sent before is shorter than the count",
-  })
-  private async say(command: CommandMessage): Promise<void> {
-    const { tempPerson, tempCount } = command.args;
-    const person: string = Validate.user(tempPerson)
-      ? Validate.userStringParser(tempPerson)
-      : Validate.userStringParser(command.author.id);
-    const count: number = Validate.checkNumber(tempCount)
-      ? parseInt(tempCount + '')
-      : 5;
+  // @Slash('say', {description: "Speaks randomly generated sentence depends on the mentioned person's chat history"})
+  //   // detail: `\`$say <@Mention> <Count?>\``,
+  // private async say(interaction: CommandInteraction): Promise<void> {
+  //   const { tempPerson, tempCount } = interaction.args;
+  //   const person: string = Validate.user(tempPerson)
+  //     ? Validate.userStringParser(tempPerson)
+  //     : Validate.userStringParser(interaction.author.id);
+  //   const count: number = Validate.checkNumber(tempCount)
+  //     ? parseInt(tempCount + '')
+  //     : 5;
 
-    const tempMessageHolder = [];
-    const getHistory = await db
-      .collection('Member')
-      .doc(person)
-      .collection('Messages')
-      .orderBy('createdAt', 'desc')
-      .limit(150)
-      .get();
+  //   const tempMessageHolder = [];
+  //   const getHistory = await db
+  //     .collection('Member')
+  //     .doc(person)
+  //     .collection('Messages')
+  //     .orderBy('createdAt', 'desc')
+  //     .limit(150)
+  //     .get();
 
-    if (!getHistory.empty) {
-      getHistory.forEach((r) => {
-        tempMessageHolder.push(r.data().message);
-      });
+  //   if (!getHistory.empty) {
+  //     getHistory.forEach((r) => {
+  //       tempMessageHolder.push(r.data().message);
+  //     });
 
-      const messagesToLearn: Array<string> = Markov.wordsFilter(
-        tempMessageHolder,
-        count
-      );
+  //     const messagesToLearn: Array<string> = Markov.wordsFilter(
+  //       tempMessageHolder,
+  //       count
+  //     );
 
-      if (messagesToLearn.length < 5) {
-        command.channel.send(
-          'Need more message histories to generate the message'
-        );
-        return;
-      }
+  //     if (messagesToLearn.length < 5) {
+  //       interaction.channel.send(
+  //         'Need more message histories to generate the message'
+  //       );
+  //       return;
+  //     }
 
-      const markov = new Markov();
+  //     const markov = new Markov();
 
-      markov.addState(messagesToLearn);
-      markov.train();
-      command.channel.send(markov.generate(count));
-    } else {
-      command.channel.send('Invalid user');
-    }
-  }
-
-  @Command('sex :tempA :tempB')
-  @Infos({
-    command: 'sex',
-    detail: '$sex <@Mention 1> <@Mention 2>',
-    description:
-      '* Make them have sex, and produce a baby\n* With a little chatters',
-  })
-  private async sex(command: CommandMessage) {
-    const { tempA, tempB } = command.args;
-    const A = Validate.userValidateAndParse(tempA);
-    const B = Validate.userValidateAndParse(tempB);
-    // const markovA = new Markov();
-    // const markovB = new Markov();
-
-    try {
-      const dataA = await db
-        .collection('Member')
-        .doc(A)
-        .collection('Messages')
-        .orderBy('createdAt', 'desc')
-        .limit(150)
-        .get();
-      const dataB = await db
-        .collection('Member')
-        .doc(B)
-        .collection('Messages')
-        .orderBy('createdAt', 'desc')
-        .limit(150)
-        .get();
-      const tempMessageHolderA = [];
-      const tempMessageHolderB = [];
-
-      if (!dataA.empty && !dataB.empty) {
-        dataA.forEach((r) => {
-          tempMessageHolderA.push(r.data().message);
-        });
-        dataB.forEach((r) => {
-          tempMessageHolderB.push(r.data().message);
-        });
-      }
-
-      // command.guild.members.cache.get(A).nickname
-
-      if (A && B) {
-        //
-      } else {
-        command.channel.send('You need a pair to produce a baby 😞');
-      }
-    } catch (error) {
-      console.error(`[${new Date()}] ${error}`);
-    }
-  }
+  //     markov.addState(messagesToLearn);
+  //     markov.train();
+  //     interaction.channel.send(markov.generate(count));
+  //   } else {
+  //     interaction.channel.send('Invalid user');
+  //   }
+  // }
 }
