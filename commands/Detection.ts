f870cfb17c6520f97b125c8a959d9ebc0ff04339
titancube <kbin0717@gmail.com>
2021-09-28 15:00:23 +0900

    refactor: rewrite code

--- commands/Detection.ts
+++ commands/Detection.ts
@@ -1,16 +1,16 @@
-import { ArgsOf, Guard, On } from "@typeit/discord";
-import { NotBot } from "../guards/NotBot";
+import { ArgsOf, Guard, On } from 'discordx';
+import { NotBot } from '../guards/NotBot';
 
 export abstract class Detection {
-  @On("message")
+  @On('message')
   @Guard(NotBot) // Message author should not be the bot
-  private async textWithoutInfo([msg]: ArgsOf<"message">): Promise<void> {
+  private async textWithoutInfo([msg]: ArgsOf<'messageCreate'>): Promise<void> {
     // Check the channel - set to #resources-n-sales / "827736673777090620"
     // Check if the message author has admin permission
     // Stop if at least one of both is true
     if (
-      msg.channel.id !== "827736673777090620" ||
-      msg.member.hasPermission("ADMINISTRATOR", { checkAdmin: true })
+      msg.channel.id !== '827736673777090620' ||
+      msg.member.permissions.has('ADMINISTRATOR', true)
     ) {
       return;
     }
@@ -22,22 +22,23 @@ export abstract class Detection {
       : // Message does not have a link or an attachment. Delete the message and send notification through DM
         (msg.delete(),
         await msg.author.send(
-          "Messages on the `#resources-n-sales` should have at least one attachment or link!\nIf this problem keeps occuring please contact **blob(Titancube)** on the server."
+          'Messages on the `#resources-n-sales` should have at least one attachment or link!\nIf this problem keeps occuring please contact **blob(Titancube)** on the server.'
         ));
   }
 
   // Detect if the message has link
-  private static hasLink([msg]: ArgsOf<"message">) {
+  private static hasLink([msg]: ArgsOf<'messageCreate'>) {
     // Return true if index of detected url is bigger than -1. String.prototype.search() returns -1 when it finds nothing
     return msg.content.search(Detection.urlRegex) > -1 ? true : false;
   }
 
   // Detect if the message has attachment
-  private static hasAttachment([msg]: ArgsOf<"message">) {
+  private static hasAttachment([msg]: ArgsOf<'messageCreate'>) {
     // Return true if attachment count is bigger than 0
     return msg.attachments.size > 0 ? true : false;
   }
 
   // Regex for link detection
-  static urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
+  static urlRegex =
+    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
 }
